4fe6568149978934249708e9b00d457f98802b9c
adriandeptula <adriandeptula@github.com>
2020-03-25 22:03:01 +0100

    fixed display, deleted unnecessary code

--- scripts/style.js
+++ scripts/style.js
@@ -37,7 +37,7 @@ $('.reviews-selection-container').slick({
     slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 1240,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 16,
           slidesToScroll: 4,
@@ -80,39 +80,3 @@ $('.reviews-selection-container').slick({
       },
     ]
   });
-
-
-  // (function(window, document, undefined) {
-  //   "use strict";
-  
-  //   // List of Video Vendors embeds you want to support
-  //   var players = ['iframe[src*="youtube.com"]', 'iframe[src*="vimeo.com"]'];
-  
-  //   // Select videos
-  //   var fitVids = document.querySelectorAll(players.join(","));
-  
-  //   // If there are videos on the page...
-  //   if (fitVids.length) {
-  //     // Loop through videos
-  //     for (var i = 0; i < fitVids.length; i++) {
-  //       // Get Video Information
-  //       var fitVid = fitVids[i];
-  //       var width = fitVid.getAttribute("width");
-  //       var height = fitVid.getAttribute("height");
-  //       var aspectRatio = height / width;
-  //       var parentDiv = fitVid.parentNode;
-  
-  //       // Wrap it in a DIV
-  //       var div = document.createElement("div");
-  //       div.className = "fitVids-wrapper";
-  //       div.style.paddingBottom = aspectRatio * 100 + "%";
-  //       parentDiv.insertBefore(div, fitVid);
-  //       fitVid.remove();
-  //       div.appendChild(fitVid);
-  
-  //       // Clear height/width from fitVid
-  //       fitVid.removeAttribute("height");
-  //       fitVid.removeAttribute("width");
-  //     }
-  //   }
-  // })(window, document);
